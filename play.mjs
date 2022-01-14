6ffe06f25263667e66820cc1bd7c6c7482933cf3
Jack Morrissey <jack@tryce.tech>
2022-01-13 20:43:10 -0600

    fix(play): modified yellow/black square result
    Based on playing today. It only will give you a yellow for the same number
    of open hints and will be black if the count is all used by green or
    yellows. Just see the tests, I don't feel like making sense here.

--- play.mjs
+++ play.mjs
@@ -35,13 +35,33 @@ export class Game {
 
 export function getResponse(test, answer) {
   const response = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < answer.length; i++) {
+    response.push("?");
+  }
+  const notFound = [];
+  for (let i = 0; i < answer.length; i++) {
     if (test[i] === answer[i]) {
-      response.push("🟩");
-    } else if (answer.includes(test[i])) {
-      response.push("🟨");
+      response[i] = "🟩";
+    } else if (!answer.includes(test[i])) {
+      notFound.push(answer[i]);
+      response[i] = "⬛";
     } else {
-      response.push("⬛");
+      notFound.push(answer[i]);
+    }
+  }
+
+  // Account for double letters. Wordle only does yellow for the count of any remaining unfound letters
+  // e.g. guess of Creed for the answer Smear will have a green e and then a black e. Likewise for single yellow
+  for (let i = 0; i < answer.length; i++) {
+    if (response[i] !== "?") {
+      continue;
+    }
+    const indexOfFound = notFound.indexOf(test[i]);
+    if (indexOfFound > -1) {
+      response[i] = "🟨";
+      notFound.splice(indexOfFound, 1);
+    } else {
+      response[i] = "⬛";
     }
   }
   return response.join("");
--- test/play.mjs
+++ test/play.mjs
@@ -5,6 +5,13 @@ tap.equal(getResponse("snake", "drink"), "⬛🟨⬛🟨⬛");
 tap.equal(getResponse("drank", "drink"), "🟩🟩⬛🟩🟩");
 tap.equal(getResponse("drink", "drink"), "🟩🟩🟩🟩🟩");
 
+tap.equal(getResponse("about", "abbey"), "🟩🟩⬛⬛⬛");
+tap.equal(getResponse("abled", "abbey"), "🟩🟩⬛🟩⬛");
+tap.equal(getResponse("rinse", "abbey"), "⬛⬛⬛⬛🟨");
+tap.equal(getResponse("cheep", "abbey"), "⬛⬛⬛🟩⬛");
+tap.equal(getResponse("weave", "abbey"), "⬛🟨🟨⬛⬛");
+tap.equal(getResponse("abbey", "abbey"), "🟩🟩🟩🟩🟩");
+
 const newGame = new Game("drink");
 
 tap.equal(newGame.guess("snake"), "⬛🟨⬛🟨⬛");
